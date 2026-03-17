b254e52b055dd947af28d1b105d70f88e577ea32
FelipeDenuzzo <felipedenuzzo@gmail.com>
2026-03-17 16:57:31 -0300

    Adiciona banner de versão centralizado, pequeno e branco, no topo da janela (fora do conteúdo principal)

--- atento/src/app/layout.tsx
+++ atento/src/app/layout.tsx
@@ -27,8 +27,27 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ margin: 0, padding: 0 }}
       >
-        {children}
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            zIndex: 9999,
+            textAlign: "center",
+            fontSize: "0.75rem",
+            color: "#fff",
+            background: "transparent",
+            pointerEvents: "none",
+            fontFamily: "inherit",
+            padding: "6px 0 0 0"
+          }}
+        >
+          versão - 1.001.001.001 - 17/03/26 - 16:57
+        </div>
+        <div style={{ paddingTop: "24px" }}>{children}</div>
       </body>
     </html>
   );
